a0629944617aa846492c41597caf2c96dde65f20
danniesan <88257285+danniesan@users.noreply.github.com>
2024-08-06 13:54:20 -0400

    added AI api intregation

--- src/script.js
+++ src/script.js
@@ -1,12 +1,26 @@
-function generateAnswer(event) {
-  event.preventDefault();
-
+function displayAnswer(response) {
+  console.log("name generated");
   new Typewriter("#answer", {
-    strings: ["Generating answer..."],
+    strings: [response.data.answer],
     autoStart: true,
     delay: 70,
     cursor: "",
   });
+}
+function generateAnswer(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "bfb46b3ac41f3bd0tof60adf87306491";
+  let context =
+    "You are an expert in creating fantasy, mythical and unique names, make sure to follow user instructions.";
+  let prompt = `User instructions: Generate unique fantasy names by gender, boy or girl ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Gernerating name");
+  console.log(`Prompt:${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayAnswer);
 }
 
 let generatorFromElement = document.querySelector("#generator-form");
