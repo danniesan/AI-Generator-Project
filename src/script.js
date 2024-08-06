67db078ec47b8a15c224d9f04ab6eb1ddd4c2e5b
danniesan <88257285+danniesan@users.noreply.github.com>
2024-08-06 13:11:13 -0400

    added basic form handling and typewritter

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateAnswer(event) {
+  event.preventDefault();
 
+  new Typewriter("#answer", {
+    strings: ["Generating answer..."],
+    autoStart: true,
+    delay: 70,
+    cursor: "",
+  });
+}
+
+let generatorFromElement = document.querySelector("#generator-form");
+generatorFromElement.addEventListener("submit", generateAnswer);
